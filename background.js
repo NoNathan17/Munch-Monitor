0e2e33fae325ac94eb35dccf52299838eb15a50f
NoNathan17 <nathanong11217@gmail.com>
2024-10-05 22:01:53 -0700

    fixed scripting problems

--- background.js
+++ background.js
@@ -6,10 +6,18 @@ periodInMinutes: 180
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
   if (alarm.name === 'eatReminder') {
-    chrome.tabs.query({ currentWindow: true }, function(tabs) {
-      tabs.forEach(function(tab) {
-          chrome.tabs.sendMessage(tab.id, { action: 'spawnWidget' });
+      chrome.tabs.query({active: true, currentWindow: true }, function(tabs) {
+          tabs.forEach(function(tab) {
+              // Use chrome.scripting.executeScript to inject the content script
+              chrome.scripting.executeScript({
+                  target: { tabId: tab.id },
+                  files: ['content.js']
+              }, () => {
+                  // After content script injection, send a message to it
+                  chrome.tabs.sendMessage(tab.id, { action: 'spawnWidget' });
+              });
+          });
       });
-    });
   }
 });
+
